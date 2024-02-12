11ead7f357a3ad1c5c96e2024c80e615f36f6a12
lauwarm <10065855+lauwarm@users.noreply.github.com>
2024-02-12 20:12:55 +0100

    add css support for pwa standalone mode

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,31 @@
-/** An empty service worker! */
+const cacheName = 'files';
 
-self.addEventListener ('fetch', function(event)
+addEventListener('fetch', fetchEvent => {
+  const { request } = fetchEvent;
+  
+  if (request.method !== 'GET') {
+    return;
+  }
 
-{
+  fetchEvent.respondWith(async function() {
+    const fetchPromise = fetch(request);
 
-/** An empty fetch handler! */
+    fetchEvent.waitUntil(async function () {
+      const responseCopy = (await fetchPromise).clone();
+      const myCache = await caches.open(cacheName);
+      await myCache.put(request, responseCopy);
+    }());
 
+    if (request.mode === 'navigate') {
+      try {
+        return await fetchPromise;
+      }
+      catch {
+        return caches.match(request);
+      }
+    } else {
+      const responseFromCache = await caches.match(request);
+      return responseFromCache || fetchPromise;
+    }
+  }());
 });
